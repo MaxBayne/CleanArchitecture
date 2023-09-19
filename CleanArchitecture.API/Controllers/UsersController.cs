8d63990cb35ea736c8315fa62de9e732d6d7cd86
Mohamed Salah <maxbayne@gmail.com>
2023-09-19 20:59:46 +0300

    finish delete action in users controlers

--- CleanArchitecture.API/Controllers/UsersController.cs
+++ CleanArchitecture.API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
-﻿using CleanArchitecture.Application.CQRS.Mediators.Requests.User.Queries;
+﻿using CleanArchitecture.Application.CQRS.Mediators.Requests.User.Commands;
+using CleanArchitecture.Application.CQRS.Mediators.Requests.User.Queries;
 using CleanArchitecture.Application.ObjectMapping.AutoMapper.Dtos.User;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,54 +27,164 @@ namespace CleanArchitecture.API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<UserDto>>> Get()
         {
-            //using Mediator to send request and mediator will handle it by handler and return the response
-            var request = new GetUserListQueryRequest();
-            var response = await _mediator.Send(request);
-            
-            if (!response.IsSuccess)
+            try
             {
-                //On Response Failed
-                return BadRequest(response.Message);
-            }
-            else
-            {
-                //On Response Success
+                //using Mediator to send request and mediator will handle it by handler and return the response
+                var request = new GetUserListQueryRequest();
+                var response = await _mediator.Send(request);
 
-                if (!response.QueriedData!.Any())
+                if (!response.IsSuccess)
                 {
-                    return NoContent();
+                    //On Response Failed
+                    return BadRequest(response.Message);
                 }
+                else
+                {
+                    //On Response Success
 
-                return Ok(response.QueriedData);
+                    if (!response.QueriedData!.Any())
+                    {
+                        return NoContent();
+                    }
+
+                    return Ok(response.QueriedData);
+                }
             }
-
-
-            
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<UserDto>> Get(int id)
         {
-            return "value";
+            try
+            {
+                //using Mediator to send request and mediator will handle it by handler and return the response
+                var request = new GetUserQueryRequest() 
+                {
+                    UserId=id 
+                };
+
+                var response = await _mediator.Send(request);
+
+                if (!response.IsSuccess)
+                {
+                    //On Response Failed
+                    return BadRequest(response.Message);
+                }
+                else
+                {
+                    //On Response Success
+
+                    if (response.QueriedData==null)
+                    {
+                        return NoContent();
+                    }
+
+                    return Ok(response.QueriedData);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<UserDto>> Post([FromBody] CreateUserDto newUser)
         {
+            try
+            {
+                //using Mediator to send request and mediator will handle it by handler and return the response
+                var command = new CreateUserCommandRequest()
+                {
+                    CreateUserDto = newUser
+                };
+
+                var response = await _mediator.Send(command);
+
+                if (!response.IsSuccess)
+                {
+                    //On Response Failed
+                    return BadRequest(response.Message);
+                }
+                else
+                {
+                    //On Response Success
+                    return CreatedAtAction(nameof(Get),new {id=response.CreatedData!.Id},response.CreatedData);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] UpdateUserDto updatedUser)
         {
+            try
+            {
+                //using Mediator to send request and mediator will handle it by handler and return the response
+                var command = new UpdateUserCommandRequest()
+                {
+                    UserId = id,
+                    UpdatedUserDto= updatedUser
+                };
+
+                var response = await _mediator.Send(command);
+
+                if (!response.IsSuccess)
+                {
+                    //On Response Failed
+                    return BadRequest(response.Message);
+                }
+                else
+                {
+                    //On Response Success
+                    return NoContent();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         // DELETE api/<UsersController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            try
+            {
+                //using Mediator to send request and mediator will handle it by handler and return the response
+                var command = new DeleteUserCommandRequest()
+                {
+                    UserId = id
+                };
+
+                var response = await _mediator.Send(command);
+
+                if (!response.IsSuccess)
+                {
+                    //On Response Failed
+                    return BadRequest(response.Message);
+                }
+                else
+                {
+                    //On Response Success
+                    return  NoContent();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
