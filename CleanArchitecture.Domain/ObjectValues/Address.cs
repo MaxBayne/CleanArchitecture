95a4989e974c3c3c0ef2cdf71b5ce07b813c82ee
Mohammed Salah <maxbayne@gmail.com>
2023-09-19 14:58:40 +0300

    create Address Entity Configuration

--- CleanArchitecture.Domain/ObjectValues/Address.cs
+++ CleanArchitecture.Domain/ObjectValues/Address.cs
@@ -4,5 +4,9 @@ namespace CleanArchitecture.Domain.ObjectValues
 {
     public class Address: BaseObjectValue
     {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Street { get; set; }  
     }
 }
---
+++ CleanArchitecture.Persistence/Configurations/AddressEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.ObjectValues;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Persistence.Configurations
+{
+    public class AddressEntityConfiguration:IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
