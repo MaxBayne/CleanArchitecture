f807dc67ad19cc0497103bf68417c7f12145f262
Mohammed Salah <maxbayne@gmail.com>
2023-09-19 14:35:12 +0300

    change the ceatedby,updatedby to int

--- CleanArchitecture.Domain/Bases/BaseEntity.cs
+++ CleanArchitecture.Domain/Bases/BaseEntity.cs
@@ -1,13 +1,11 @@
-﻿using CleanArchitecture.Domain.Entities;
-
-namespace CleanArchitecture.Domain.Bases
+﻿namespace CleanArchitecture.Domain.Bases
 {
     public abstract class BaseEntity
     {
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
 
-        public User? CreatedBy {get;set;}
-        public User? UpdatedBy { get; set; }
+        public int CreatedBy {get;set;}
+        public int UpdatedBy { get; set; }
     }
 }
