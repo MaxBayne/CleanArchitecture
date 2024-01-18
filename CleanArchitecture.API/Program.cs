7c4612f59d100edd4d12dc207f348b9fc2b353e0
Eng.Mohammed Salah <maxbayne@gmail.com>
2024-01-18 20:44:50 +0200

    convert program.cs to minmial style for api

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,50 +1,42 @@
-using CleanArchitecture.API.Infrastructure;
 using CleanArchitecture.Application;
 using CleanArchitecture.Identity;
 using CleanArchitecture.Infrastructure;
 using CleanArchitecture.Persistence;
+using CleanArchitecture.API.Infrastructure;
+using CleanArchitecture.API;
 
 
-namespace CleanArchitecture.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Config services to the container.
+builder.Services.AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration)
+                .AddPersistenceServices(builder.Configuration)
+                .AddIdentityServices(builder.Configuration)
+                .AddApiServices();
+
+// Build Application
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. (Middleware)
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Config services to the container.
-            builder.Services.AddApplicationServices()
-                            .AddInfrastructureServices(builder.Configuration)
-                            .AddPersistenceServices(builder.Configuration)
-                            .AddIdentityServices(builder.Configuration)
-                            .AddApiServices();
-
-            // Build Application
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline. (Middleware)
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("CorsPolicy");
-
-            //Configure Security
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            //Map Endpoints
-            app.UseRouting();
-            app.MapControllers(); //Endpoints over Controller classes
-            app.MapEndpoints(); //Endpoints inside files
-
-            //Start Application
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+//Configure Security
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Map Endpoints
+app.UseRouting();
+app.MapControllers(); //Endpoints over Controller classes
+app.MapEndpoints(); //Endpoints inside files
+
+//Start Application
+app.Run();
