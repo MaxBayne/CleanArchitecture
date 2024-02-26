e25b65e660081832c3aafd9cc8b8c674d2d2e3cc
Mohammed Salah <maxbayne@gmail.com>
2024-02-26 11:36:33 +0200

    create new api controller for test

---
+++ CleanArchitecture.API/Controllers/InfoController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using CleanArchitecture.API.Attributes;
+using CleanArchitecture.Application.ObjectMapping.AutoMapper.Dtos.Book;
+
+// ReSharper disable NotAccessedField.Local
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class InfoController : ControllerBase
+    {
+        private readonly ILogger<InfoController> _logger;
+        private readonly IMediator _mediator;
+
+        public InfoController(ILogger<InfoController> logger,IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+
+        // GET: api/<BookController>
+        [HttpGet]
+        [ResponseType(typeof(List<ViewBookDto>), StatusCodes.Status200OK)]
+        [ResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            try
+            {
+                //_logger.LogInformation("Info Controller");
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+       
+    }
+}
