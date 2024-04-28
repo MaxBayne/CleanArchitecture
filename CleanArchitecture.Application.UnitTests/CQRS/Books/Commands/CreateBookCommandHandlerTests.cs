e5666aaa015813c653047732a97137bf75f1090c
Mohammed Salah <maxbayne@gmail.com>
2024-04-28 10:03:42 +0300

    arrange unit tests for createbookcommandhandlerTests

--- CleanArchitecture.Application.UnitTests/CQRS/Books/Commands/CreateBookCommandHandlerTests.cs
+++ CleanArchitecture.Application.UnitTests/CQRS/Books/Commands/CreateBookCommandHandlerTests.cs
@@ -1,5 +1,4 @@
-﻿using Moq;
-using AutoMapper;
+﻿using AutoMapper;
 using CleanArchitecture.Application.Interfaces.Persistence.Abstract;
 using FluentAssertions;
 using CleanArchitecture.Application.Interfaces.Persistence.Repositories;
@@ -8,38 +7,24 @@ using CleanArchitecture.Application.Mediators.CQRS.Book.Commands;
 using CleanArchitecture.Application.ObjectMapping.AutoMapper.Dtos.Book;
 using CleanArchitecture.Application.ObjectMapping.AutoMapper.Profiles;
 using CleanArchitecture.Domain.Entities;
-
+using FakeItEasy;
 
 namespace CleanArchitecture.Application.UnitTests.CQRS.Books.Commands;
 
 
 public class CreateBookCommandHandlerTests
 {
-    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
-    private readonly Mock<IBookRepository> _bookRepositoryMock;
-    private readonly Mock<INotificationPublisher> _notificationPublisherMock;
-    private readonly IMapper _mapper;
-
-    public CreateBookCommandHandlerTests()
-    {
-        _unitOfWorkMock=new();
-        _bookRepositoryMock = new();
-        _notificationPublisherMock = new();
-         
-
-        //Setup AutoMapper
-        _mapper = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile<MappingProfile>();
-        }).CreateMapper();
-    }
-
     [Fact]
-    public async Task Handle_WhenValidationFail_ShouldReturnFailureResult()
+    public async Task Handle_When_ValidationFail_ShouldReturn_FailureResult()
     {
         //Arrange
 
-        var handler = new CreateBookCommandHandler(_bookRepositoryMock.Object, _unitOfWorkMock.Object, _mapper, _notificationPublisherMock.Object);
+        var unitOfWorkFake = A.Fake<IUnitOfWork>();
+        var bookRepositoryFake = A.Fake<IBookRepository>();
+        var notificationPublisherFake = A.Fake<INotificationPublisher>();
+        var mapper = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); }).CreateMapper();
+
+        var handler = new CreateBookCommandHandler(bookRepositoryFake, unitOfWorkFake, mapper, notificationPublisherFake);
 
         //make validation fail
         var command = new CreateBookCommand(new CreateBookDto { Title = "", Category = "", IsActive = true });
@@ -56,11 +41,15 @@ public class CreateBookCommandHandlerTests
     }
 
     [Fact]
-    public async Task Handle_WhenValidationSuccess_ShouldNotReturnFailureResult()
+    public async Task Handle_When_ValidationSuccess_ShouldReturn_NotFailureResult()
     {
         //Arrange
+        var unitOfWorkFake = A.Fake<IUnitOfWork>();
+        var bookRepositoryFake = A.Fake<IBookRepository>();
+        var notificationPublisherFake = A.Fake<INotificationPublisher>();
+        var mapper = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); }).CreateMapper();
 
-        var handler = new CreateBookCommandHandler(_bookRepositoryMock.Object, _unitOfWorkMock.Object, _mapper, _notificationPublisherMock.Object);
+        var handler = new CreateBookCommandHandler(bookRepositoryFake, unitOfWorkFake, mapper, notificationPublisherFake);
 
         //make validation success
         var command = new CreateBookCommand(new CreateBookDto { Title = "Learning OOP", Category = "Programming", IsActive = true });
@@ -76,24 +65,25 @@ public class CreateBookCommandHandlerTests
     }
 
     [Fact]
-    public async Task Handle_WhenValidationSuccess_ShouldReturnSuccessResultWithValue()
+    public async Task Handle_When_ValidationSuccess_ShouldReturn_SuccessResultWithValue()
     {
         //Arrange
-
+        var unitOfWorkFake = A.Fake<IUnitOfWork>();
+        var bookRepositoryFake = A.Fake<IBookRepository>();
+        var notificationPublisherFake = A.Fake<INotificationPublisher>();
         var newBookDto = new CreateBookDto { Title = "Learning OOP", Category = "Programming", IsActive = true };
         var newBookEntity = Book.Create(1, "Learning OOP","", "Programming", true);
+        var mapper = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); }).CreateMapper();
 
         //Setup BookRepository Methods
-        _bookRepositoryMock.Setup(x => x.AddAsync(newBookEntity, true)).ReturnsAsync(newBookEntity);
+        A.CallTo(() => bookRepositoryFake.AddAsync(newBookEntity, true)).Returns(newBookEntity);
 
-        var handler = new CreateBookCommandHandler(_bookRepositoryMock.Object, _unitOfWorkMock.Object, _mapper, _notificationPublisherMock.Object);
+        var handler = new CreateBookCommandHandler(bookRepositoryFake, unitOfWorkFake, mapper, notificationPublisherFake);
 
         var command = new CreateBookCommand(newBookDto);
-        
 
         //Act
         var result = await handler.Handle(command, default);
-
 
         //Assert
         result.Value.Should().NotBeNull();
