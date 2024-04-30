1e9fa46c14900c46e9991e76074e4179b461d903
Mohammed Salah <maxbayne@gmail.com>
2024-04-30 10:57:51 +0300

    enable interactive server render mode inside blazor app

--- CleanArchitecture.Blazor/Program.cs
+++ CleanArchitecture.Blazor/Program.cs
@@ -9,10 +9,22 @@ namespace CleanArchitecture.Blazor
     {
         public static void Main(string[] args)
         {
+
+            //Blazor Render Modes
+            //--------------------
+            //1- Static Server Side Render : its Default Render Mode For Blazor Components (Mean Blazor Make Request to Server and Wait For Response)
+            //2- Interactive Server Side Render : mean browser open websocket between browser and server and any reaction made for UI will be send to Server Via WebSocket and Take Response
+            //3- Client Side Render : mean Dot Net Runtime and App Bundles will be Downloaded and Cached inside Browser and Render will be made via Web Browser by Web Assembly , its perfect for offline apps
+            //4- Automatic Render : mean will get benefits from interactive server render and Client Side Render mean first time will get component from interactive server render and on each sub request from component will be render via Client Render using Web Assembly
+
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services (Dependency Injection) to the container.
-            builder.Services.AddRazorComponents();
+            builder.Services.AddRazorComponents()
+                            .AddInteractiveServerComponents()//enable interactive Server Side Render (Web Socket)
+                            ;
+            
 
             //Register Clients
             builder.Services.AddSingleton<IGamesClient,GamesClient>();
@@ -40,7 +52,9 @@ namespace CleanArchitecture.Blazor
             app.UseStaticFiles();
             app.UseAntiforgery();
 
-            app.MapRazorComponents<App>();
+            app.MapRazorComponents<App>()
+               .AddInteractiveServerRenderMode() //Enable Interactive Server Render Mode
+               ; 
 
             app.Run();
         }
