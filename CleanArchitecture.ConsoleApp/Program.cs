0746f8fa0799412dd00cf1caebf206e0834d626c
Eng.Mohammed Salah <maxbayne@gmail.com>
2024-05-16 11:29:27 +0300

    creating new project as console app

---
+++ CleanArchitecture.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanArchitecture.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss"));
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
